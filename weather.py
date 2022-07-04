0049dd4345f2606916f5b6903f519fbaf27c980f
MahdiKanaany <mkanani13882009@gmai.com>
2022-07-04 13:56:37 +0430

    added get input from user and translate the weather in weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
 import requests as re
 import urllib.parse
+from googletrans import Translator, constants
+translator = Translator()
 
-address = 'tehran'
+address = input('Please give me your city: ')
 url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
 
 response = re.get(url).json()
@@ -10,4 +12,7 @@ long = response[0]['lon']
 url2 = 'https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=dfc43f517f1994091fa59846428d7f50' %(lat,long)
 
 r = re.get(url2)
-print(r.json()['weather'][0]['description'])
+w = r.json()['weather'][0]['description']
+translation = translator.translate(w, dest="fa")
+print(translation.text)
+
